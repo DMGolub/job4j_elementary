3a176eb3ac7bcc61d165de9f404b3a1a500a2f22
Dmitry Golub <dmgolub@mail.ru>
2022-07-18 23:14:51 +0300

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -6,8 +6,16 @@ public class Converter {
         return value / 70;
     }
 
+    public static float euroToRuble(float value) {
+        return value * 70;
+    }
+
     public static float rubleToDollar(float value) {
         return value / 60;
+    }
+
+    public static float dollarToRuble(float value) {
+        return value * 60;
     }
 
     public static void main(String[] args) {
